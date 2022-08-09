24bfe05889015be4a3e76ba5f4fdc67d8c2991d6
Alvin Accad <alvinaccad5@gmail.com>
2022-08-09 15:49:21 -0700

    apartment resource

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Apartment, type: :model do
   it 'check that Apartment exists' do
     user = User.where(email: 'test@example.com').first_or_create(password:'12345678', password_confirmation:'123456789')
 
-   apartment = Apartment.create city: 'San Diego', state: 'CA', manager: 'Elyse', email: 'elyse_manager@email.com', price: '3000', bedrooms: 1, bathrooms: 1, pets: '0', image: 'image', user_id: user.id
+    apartment = user.apartments.create city: 'San Diego', state: 'CA', manager: 'Elyse', email: 'elyse_manager@email.com', price: '3000', bedrooms: 1, bathrooms: 1, pets: '0', image: 'image'
     expect(apartment.errors[:street]).to_not be_empty
   end
 end
