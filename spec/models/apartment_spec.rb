710c23feda62b07f0e35354f0a22e12f0659861a
LeoContreras93 <Contreras.leo2211@gmail.com>
2022-08-09 15:25:20 -0700

    rspec test changes

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
   it 'check that Apartment exists' do
-    apartment = Apartment.create city: 'San Diego', state: 'CA', manager: 'Elyse', email: 'elyse_manager@email.com', price: '3000', bedrooms: 1, bathrooms: 1, pets: '0', image: 'image', user_id: 1
+    user = User.where(email: 'test@example.com').first_or_create(password:'12345678', password_confirmation:'123456789')
+
+   apartment = Apartment.create city: 'San Diego', state: 'CA', manager: 'Elyse', email: 'elyse_manager@email.com', price: '3000', bedrooms: 1, bathrooms: 1, pets: '0', image: 'image', user_id: user.id
     expect(apartment.errors[:street]).to_not be_empty
   end
 end
