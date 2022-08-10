e091c2d9718922c99f80e461f6ca4de60c60738f
Segjr <samuel8016@gmail.com>
2022-08-10 15:53:01 -0700

    unprotected index updated

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react'
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 export default class ApartmentIndex extends Component {
+
   render() {
     return (
-      <h3>ApartmentIndex</h3>
+      <>
+        <h3>ApartmentIndex</h3>
+        <div>
+        { this.props.apartments && this.props.apartments.map(apartment => {
+          return(
+            <Col>
+              <Card style={{width: '18rem'}} key={apartment.id} >
+                <CardImg top width= "100%" src={apartment.image} alt="Card image cap" />
+                <ListGroup flush>
+                  <ListGroupItem>
+                    {apartment.price}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    {apartment.street}
+                    {apartment.city}, {apartment.state}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                   Beds {apartment.bedrooms}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                   Baths {apartment.bathrooms}
+                  </ListGroupItem>
+                </ListGroup>
+                <CardBody>
+                  <NavLink to={`/apartmentshow/${apartment.id}`}>
+                    <Button>Details</Button>
+                  </NavLink>
+                </CardBody>
+              </Card>
+          </Col>
+          )
+        })} 
+        </div>
+      </>
     )
   }
 }
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -2,6 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of apartments" do
+      Apartment.create(
+        street: '4341 Spring St',
+        city: 'La Mesa',
+        state: 'CA',
+        manager: 'John',
+        email: 'John@gmail.com',
+        price: '1900',
+        bedrooms: 1,
+        bathrooms: 1,
+        pets: 'no',
+        image: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1380&q=80'
+      )
+
+      get '/apartments/'
+      apartment = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(apartment.length).to eq 0
+    end
   end
 end
