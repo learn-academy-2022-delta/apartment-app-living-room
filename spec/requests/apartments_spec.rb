5569b64a709f4942a4f2e866e7ba8effc64b696f
Alvin Accad <alvinaccad5@gmail.com>
2022-08-10 11:58:31 -0700

    validations completed

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "get a list of apartments" do
+        user = User.where(email: 'test@example.com').first_or_create(password:'12345678', password_confirmation:'12345678')
+        user.apartments.create street: "1234 Street Lane", city: 'San Diego', state: 'CA', manager: 'Elyse', email: 'elyse_manager@email.com', price: '3000', bedrooms: 1, bathrooms: 1, pets: '0', image: 'image', user_id: 1
+        get '/apartments'
+        apartment = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(apartment.length).to eq 1
+      end    
   end
 end
