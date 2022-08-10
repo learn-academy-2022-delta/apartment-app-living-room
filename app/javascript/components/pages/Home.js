12c066b4934965d570161f15051daf74608949f4
LeoContreras93 <Contreras.leo2211@gmail.com>
2022-08-10 11:19:30 -0700

    Home-page complete

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
-    )
+      <>
+        <h1>Welcome</h1>
+        <h3>
+          Discover your new home search our available apartment listings. You'll
+          find your next home here!
+        </h3>
+        <Button>Sign-In</Button>
+        <Button>Sign-Out</Button>
+      </>
+    );
   }
 }
