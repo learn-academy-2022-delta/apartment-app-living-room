c8b9dc3b13e0df4bc4cade9248514ba5ff521f23
Segjr <samuel8016@gmail.com>
2022-08-11 13:10:57 -0700

    unprotected show update 2

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -9,21 +9,21 @@ export default class ApartmentShow extends Component {
       <>
       <h3>ApartmentShow</h3>
       <Card>
-                    <CardImg top width="100%" src={apartment.image} alt="Card image cap" />
-                    <CardBody>
-                        <CardTitle>{apartment.street}</CardTitle>
-                        <CardSubtitle>{apartment.city}, {apartment.state}</CardSubtitle>
-                        <ListGroup flush>
-                          <ListGroupItem>
-                            {apartment.manager}
-                            {apartment.email}
-                            {apartment.bedrooms}
-                            {apartment.bathrooms}
-                            {apartment.pets}
-                          </ListGroupItem>
-                        </ListGroup>
-                    </CardBody>
-                </Card>
+        <CardImg top width="100%" src={apartment.image} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{apartment.street}</CardTitle>
+            <CardSubtitle>{apartment.city}, {apartment.state}</CardSubtitle>
+              <ListGroup flush>
+                <ListGroupItem>
+                  {apartment.manager}
+                  {apartment.email}
+                  {apartment.bedrooms}
+                  {apartment.bathrooms}
+                  {apartment.pets}
+                </ListGroupItem>
+              </ListGroup>
+          </CardBody>
+      </Card>
       </>
     )
   }
